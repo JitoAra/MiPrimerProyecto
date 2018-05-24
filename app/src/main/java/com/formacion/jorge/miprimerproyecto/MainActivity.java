87316a7ed477fa5d4693c4aecf9137dc272c127f
JitoAra <jorgearacilgomez.com>
2018-05-24 21:15:16 +0200

    Initial commit

---
+++ app/src/main/java/com/formacion/jorge/miprimerproyecto/MainActivity.java
@@ -0,0 +1,13 @@
+package com.formacion.jorge.miprimerproyecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
